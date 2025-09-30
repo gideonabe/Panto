55c83c7ef66685cb23cf63e2a299acd3cb2c6a0f
Gideon Abe <gideonabe2020@gmail.com>
2025-09-30 14:10:23 +0100

    Assets added

--- src/constants/image.js
+++ src/constants/image.js
@@ -1,7 +1,35 @@
-import bg from '../assets/background.png';
+import bg from '@/assets/background.png';
+import exp from '@/assets/experience.png';
+import material1 from '@/assets/material1.png';
+import material2 from '@/assets/material2.png';
+import material3 from '@/assets/material3.png';
+import review1 from '@/assets/review1.png';
+import review2 from '@/assets/review2.png';
+import review3 from '@/assets/review3.png';
+import chair1 from '@/assets/chair1.png';
+import chair2 from '@/assets/chair2.png';
+import chair3 from '@/assets/chair3.png';
+import chair4 from '@/assets/chair4.png';
+import person1 from '@/assets/person1.png';
+import person2 from '@/assets/person2.png';
+import person3 from '@/assets/person3.png';
 
 const images = {
   bg,
+  exp,
+  material1,
+  material2,
+  material3,
+  review1,
+  review2,
+  review3,
+  chair1,
+  chair2,
+  chair3,
+  chair4,
+  person1,
+  person2,
+  person3
 }
 
 export default images
