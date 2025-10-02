6315b58e2db29534cf4eb216c6472c7ae74072b5
Gideon Abe <gideonabe2020@gmail.com>
2025-10-02 02:59:29 +0100

    more

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Analytics } from '@vercel/analytics/react'
+import Head from 'next/head';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -12,13 +13,18 @@ export const metadata = {
   title: 'Panto',
   description: 'Make your interior more minimalistic & modern',
   icons: {
-    icon: '/favicon.ico'
+    icon: "/sofa.png"
   }
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/sofa.png" />
+      </Head>
       <body className={`${inter.variable} antialiased`}>
         {children}
         <Analytics />
--- src/components/Whychoose.jsx
+++ src/components/Whychoose.jsx
@@ -27,7 +27,7 @@ const Whychoose = () => {
       <div className='grid grid-cols-1 md:grid-cols-4 gap-8'>
         {/* Column 1 */}
         <div className='col-span-1'>
-          <p className='text-black font-bold text-2xl md:text-5xl items-center justify-center'>Why Choosing Us</p>
+          <p className='text-black font-bold text-2xl md:text-4xl items-center justify-center'>Why Choosing Us</p>
         </div>
 
         {/* Columns 2, 3, 4 - one per reason */}
